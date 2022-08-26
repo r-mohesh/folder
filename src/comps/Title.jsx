d9212b7251b727f84152dab7d001a9b0178b853b
DESKTOP-AT770DD\Dell <mahesh.raj@minddeft.com>
2022-08-26 10:59:13 +0530

    changes in title h1

--- src/comps/Title.jsx
+++ src/comps/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1>Google Photos</h1>
+      <h1>Google Photos!!</h1>
       <h2>Your Pictures</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
